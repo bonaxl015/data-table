41f3a784e2bd4685ee8693347945e674e91fe56a
Bon Axl Feeser <78939695+bonaxl015@users.noreply.github.com>
2025-07-21 23:27:36 +0800

    chore(build): optimize build and proper types exposure (#7)

--- src/components/datatable/components/SearchFields/types/useSearchButtons.types.ts
+++
@@ -1,5 +0,0 @@
-import { ReactNode } from "react";
-
-export type UseSearchButtonOutput = {
-  searchButtons: ReactNode;
-};
--- src/components/datatable/types/index.ts
+++ src/components/datatable/types/index.ts
@@ -1,4 +1,2 @@
 export type * from "./DataTable.types";
 export type * from "./DataTableRef.types";
-export type * from "./useDataSource.types";
-export type * from "./useRequestData.types";
