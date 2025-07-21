f2b79af053e96b04a6e93df86755429ba00e336d
bonaxl015 <bonaxl015@gmail.com>
2025-07-21 20:34:56 +0800

    feat(jest): unit test setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,28 @@
 
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
+import react from "@vitejs/plugin-react";
 import { peerDependencies } from "./package.json";
 
 export default defineConfig({
   build: {
     lib: {
-      entry: "./src/index.ts", // Specifies the entry point for building the library.
-      name: "vite-react-ts-button", // Sets the name of the generated library.
-      fileName: (format) => `index.${format}.js`, // Generates the output file name based on the format.
-      formats: ["cjs", "es"], // Specifies the output formats (CommonJS and ES modules).
+      entry: "./src/index.ts",
+      name: "vite-react-ts-button",
+      fileName: (format) => `index.${format}.js`,
+      formats: ["cjs", "es"],
     },
     rollupOptions: {
-      external: [...Object.keys(peerDependencies)], // Defines external dependencies for Rollup bundling.
+      external: [...Object.keys(peerDependencies)],
     },
-    sourcemap: true, // Generates source maps for debugging.
-    emptyOutDir: true, // Clears the output directory before building.
+    sourcemap: true,
+    emptyOutDir: true,
   },
-  plugins: [dts()], // Uses the 'vite-plugin-dts' plugin for generating TypeScript declaration files (d.ts).
+  plugins: [dts(), react()],
   test: {
-    globals: true,
+    setupFiles: "./vitest.setup.ts",
     environment: "jsdom",
-    setupFiles: "./testSetup.ts",
+    globals: true,
+    include: ["src/**/*.{test,spec}.{ts,tsx}"],
   },
 });
