68c36db0373648a40e8ac416a64aa07a153da5c1
Bon Axl Feeser <78939695+bonaxl015@users.noreply.github.com>
2025-07-25 10:34:22 +0800

    chore: added bundle analyzer (#11)
    * chore: added bundle analyzer
    
    * chore: fix setup for bundle analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import react from "@vitejs/plugin-react";
+import { analyzer } from "vite-bundle-analyzer";
+import path from "path";
 import { peerDependencies } from "./package.json";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   build: {
     lib: {
       entry: "./src/index.ts",
@@ -16,14 +18,22 @@ export default defineConfig({
     rollupOptions: {
       external: [...Object.keys(peerDependencies)],
     },
-    sourcemap: true,
+    sourcemap: mode !== "production",
     emptyOutDir: true,
   },
-  plugins: [dts(), react()],
+  plugins: [
+    dts(),
+    react(),
+    analyzer({
+      analyzerMode: "static",
+      fileName: path.resolve(process.cwd(), "bundle-analyzer.html"),
+      reportTitle: "DataTable Bundle Analyzer",
+    }),
+  ],
   test: {
     setupFiles: "./vitest.setup.ts",
     environment: "jsdom",
     globals: true,
     include: ["src/**/*.{test,spec}.{ts,tsx}"],
   },
-});
+}));
